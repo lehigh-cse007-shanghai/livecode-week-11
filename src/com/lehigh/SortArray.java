f397920986c369f6f4535efd7be574557a27855e
Lunde CHEN <lunde.chen@yahoo.fr>
2020-12-02 15:21:20 +0800

    15h24

--- src/com/lehigh/SortArray.java
+++ src/com/lehigh/SortArray.java
@@ -23,6 +23,50 @@ public class SortArray {
                     swap(j, j + 1);
     }
 
+    public void bubbleSort2(){
+        for(int i = 0; i < size; i ++) // i 指代轮次
+            for(int j = size - 1; j >= 1 + i; j --) // j 指代位置
+                if(arr[j] < arr[j - 1])
+                    swap(j, j - 1);
+    }
+
+    public void bubbleSortRecursive(int i){
+        if(i == 0)
+            return;
+        for(int j = 0; j < i; j ++)
+            if(arr[j] > arr[j + 1])
+                swap(j, j + 1);
+        bubbleSortRecursive(i - 1);
+    }
+
+    public void bubbleSortRecursive2(int i){
+        if(i == size - 1)
+            return;
+        for(int j = size - 1; j > i; j --)
+            if(arr[j] < arr[j - 1])
+                swap(j, j - 1);
+        bubbleSortRecursive2(i + 1);
+    }
+
+
+    // 1 1 2 3 5 8 13 21
+    public int fibonacciRecursive(int i){
+        if(i == 0 || i == 1)
+            return 1;
+        return fibonacciRecursive(i -1) + fibonacciRecursive(i - 2);
+    }
+
+    public int fibonacciIterative(int i) {
+        int[] arrFibo = new int[10];
+        arrFibo[0] = 1;
+        arrFibo[1] = 1;
+        for(int j = 2; j <= 9; j ++)
+            arrFibo[j] = arrFibo[j - 1] + arrFibo[j - 2];
+        return arrFibo[i];
+    }
+
+
+
     public void twoForLoops(){
         for(int i = 0; i < 5; i ++)
             for(int j = i; j < 4 - i - 1; j ++)
@@ -43,10 +87,22 @@ public class SortArray {
         }
     }
 
+    public void selectionSort2(){
+        int max_index = -1;
+        for(int i = size - 1; i >= 1; i --) {
+            max_index = i;
+            for(int j = 0; j <= i - 1; j ++)
+                if(arr[j] > arr[max_index])
+                    max_index = j;
+            swap(i, max_index);
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
         SortArray sa = new SortArray();
-
-        Arrays.sort(sa.arr);
 
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(10);
@@ -56,11 +112,15 @@ public class SortArray {
 
         Collections.sort(arrayList);
 
-        System.out.println(arrayList);
+//        System.out.println(arrayList);
 
 //        sa.twoForLoops();
-//        sa.bubbleSort();
-//        sa.selectionSort();
-//        System.out.println(Arrays.toString(sa.arr));
+//        sa.bubbleSort2();
+//        sa.selectionSort2();
+        sa.bubbleSortRecursive(3);
+//        sa.bubbleSortRecursive2(0);
+        System.out.println(Arrays.toString(sa.arr));
+
+//        System.out.println(sa.fibonacciIterative(7));
     }
 }
